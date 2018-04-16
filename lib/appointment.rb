40f10062982db5184dac28a1fc0808a26a0b148a
Sean Febo <seanfebo@techie.com>
2018-04-16 06:22:55 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,5 +1,9 @@
-class Appointment 
+class Appointment
+  attr_accessor :date, :patient, :doctor 
   def initialize(date, doctor)
-    @date = date 
+    @date = date
     @doctor = doctor 
+    doctor.add_appointment(self)
+  end
+end
     
